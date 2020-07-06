a9b946a2753725fde4ffcd7f86eee0fd4002d2f0
navigator117 <navigator117@gmail.com>
2020-07-06 10:14:11 +0800

    update build script

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
+scl enable devtoolset-7
 mkdir -p build
 cd build
 cmake ../googlemock -DBUILD_SHARED_LIBS=ON
 make
 cd ..
+mkdir -p ../../output/Debug/lib
+mkdir -p ../../output/Release/lib
 cp -rf build/lib/*.so ../../output/Debug/lib
 cp -rf build/lib/*.so ../../output/Release/lib
